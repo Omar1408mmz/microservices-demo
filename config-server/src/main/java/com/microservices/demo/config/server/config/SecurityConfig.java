2c137272f3043c88d066ef895099431c4d723b88
Omar1408mmz <38399708+Omar1408mmz@users.noreply.github.com>
2023-09-24 23:24:18 +0100

    adding encription/decryption config

---
+++ config-server/src/main/java/com/microservices/demo/config/server/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.microservices.demo.config.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/encrypt/**")
+                .antMatchers("/encrypt/**");
+        super.configure(web);
+    }
+}
